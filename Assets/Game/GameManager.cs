4c6d5b140911afe7d28c19f4fc7c500f44b9faf6
Matt Levonian <elevonian@gmail.com>
2016-11-04 21:38:42 -0700

    Basic game object type

---
+++ Assets/Game/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isPlayMode;
+
+	public void StartPlayMode()
+    {
+        if (isPlayMode)
+        {
+            foreach (ModalObject obj in GameObject.FindObjectsOfType<ModalObject>())
+            {
+                obj.StartPlayMode();
+                obj.isPlayMode = true;
+            }
+        }
+
+        foreach (ModalObject obj in GameObject.FindObjectsOfType<ModalObject>())
+        {
+            obj.ResetPlayMode();
+        }
+
+    }
+
+    public void SwitchToPlaceMode()
+    {
+        foreach (ModalObject obj in GameObject.FindObjectsOfType<ModalObject>())
+        {
+            Destroy(obj.gameObject);
+        }
+    }
+
+    public void Update()
+    {
+        if (isPlayMode)
+        {
+            foreach (ModalObject obj in GameObject.FindObjectsOfType<ModalObject>())
+            {
+                obj.UpdateInPlayMode();
+            }
+        }
+        else
+        {
+            foreach (ModalObject obj in GameObject.FindObjectsOfType<ModalObject>())
+            {
+                obj.UpdateInPlaceMode();
+            }
+        }
+    }
+}
---
+++ Assets/Game/ModalObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModalObject : MonoBehaviour
+{
+    private bool _isPlayMode;
+    public bool isPlayMode
+    {
+        get
+        {
+            return _isPlayMode;
+        }
+        set
+        {
+            _isPlayMode = value;
+        }
+    }
+
+    // called every frame in play mode
+    public virtual void UpdateInPlayMode()
+    {
+
+    }
+
+    // called every frame in place mode
+    public virtual void UpdateInPlaceMode()
+    {
+
+    }
+
+    // called once to construct anything you need for play mode
+    public virtual void StartPlayMode()
+    {
+
+    }
+
+    // called at the start of a new playthrough (including the first one
+    public virtual void ResetPlayMode()
+    {
+
+    }
+}
