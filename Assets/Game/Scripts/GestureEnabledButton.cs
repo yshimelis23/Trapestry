f29fad76d2f4ba3f09bc41362597ec4842d0592b
Matt Levonian <elevonian@gmail.com>
2016-11-05 16:10:41 -0700

    asdf

--- Assets/Game/Scripts/GestureEnabledButton.cs
+++ Assets/Game/Scripts/GestureEnabledButton.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 public class GestureEnabledButton : MonoBehaviour {
     public void OnGazeEnter()
     {
         GetComponent<Button>().Select();
+    }
+    public void OnGazeLeave()
+    {
+        if(EventSystem.current.currentSelectedGameObject == this.gameObject)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+        }
     }
     public void OnPressed()
     {
