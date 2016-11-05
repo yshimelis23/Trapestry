b19d942079856b96ac6059ddd37cd4660c0a460b
Vishnu <vishnucarbon@gmail.com>
2016-11-05 02:36:13 -0700

    Start position prefab

---
+++ Assets/Game/Scripts/ContainsTracker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class ContainsTracker : MonoBehaviour
+{
+    public event Action playerEnterCallback;
+    public event Action playerExitCallback;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MainCamera" && playerEnterCallback != null)
+        {
+            playerEnterCallback.Invoke();
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "MainCamera" && playerExitCallback != null)
+        {
+            playerExitCallback.Invoke();
+        }
+    }
+
+}
