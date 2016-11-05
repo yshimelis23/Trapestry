f9fe299ea7564e69a8de5820406ccf319798280c
aaditdoshi <aaditdos@usc.edu>
2016-11-04 22:34:40 -0700

    Added Functions to LAzer object

---
+++ Assets/Game/Scripts/Objects/LazerObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LazerObject : ModalObject {
+
+    float rotation_speed;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+    // called once to construct anything you need for play mode
+    public override void StartPlayMode()
+    {
+        rotation_speed = (Camera.main.transform.position - gameObject.transform.position).magnitude;
+    }
+
+    // called every frame in play mode
+    public override void UpdateInPlayMode()
+    {
+
+    }
+}
